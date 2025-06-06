0cf8b3156aa3968eae9764419e85215e3c92fc53
Denise Tan <denisetanumihardja@yahoo.com>
2025-06-06 03:42:54 -0700

    Made changes to arduino files, and made duplicate for non-BLE communication

--- src/ArmQuest_emg/ArmQuest_emg.ino
+++ src/ArmQuest_emg/ArmQuest_emg.ino
@@ -10,76 +10,60 @@
 // FUNCTIONS & OUTSIDE VARIABLES
 const int muscle[4]    = {EMG1, EMG2, EMG3, EMG4};
 float     emg_val[4]   = {0, 0, 0, 0};
-const int threshold[4] = {900, 900, 0, 0}; // Adjust as needed can very depending on electrode placement
+float     emg_norm[4]  = {0, 0, 0, 0};
+const int emg_min[4] = {50, 40, 0, 0}; // Adjust as needed can very depending on electrode placement
+const int emg_max[4] = {350, 600, 0, 0}; // Adjust as needed can very depending on electrode placement
 
-// Create a BLE service and a characteristic
-BLEService simpleService("180C"); // Custom service UUID
-BLEStringCharacteristic messageChar("2A56", BLERead | BLEWrite | BLENotify, 50); // Custom characteristic
+static float angle        = 0.0;
+static float emg_delta[2] = {0.0, 0.0};
 
 
 // INITIALIZATIONS
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-
-  if (!BLE.begin()) {
-    Serial.println("Starting BLE failed!");
-    while (1);
-  }
-
-  // Set the device name and local name
-  BLE.setDeviceName("Nano33BLE");
-  BLE.setLocalName("Nano33BLE");
-  BLE.setAdvertisedService(simpleService);
-
-  // Add characteristic to the service
-  simpleService.addCharacteristic(messageChar);
-
-  // Add service to BLE stack
-  BLE.addService(simpleService);
-
-  // Set initial value
-  messageChar.writeValue("Hello!");
-
-  // Start advertising
-  BLE.advertise();
-  Serial.println("BLE device is now advertising...");
 }
+
 
 // MAIN
 void loop() {
-  BLEDevice central = BLE.central();
+  emg_val[0] = analogRead(muscle[0]);
+  // emg_val[1] = analogRead(muscle[1]);
+  // emg_val[2] = analogRead(muscle[2]);
+  // emg_val[3] = analogRead(muscle[3]);
 
-  if (central) {
-    Serial.print("Connected to: ");
-    Serial.println(central.address());
+  // Clamp values
+  for (int i = 0; i < 4; i++)
+  {
+    // Clamp values
+    emg_val[i] = constrain(emg_val[i], emg_min[i], emg_max[i]);
 
-    while (central.connected()) {
-      emg_val[0] = analogRead(muscle[0]);
-      // emg_val[1] = analogRead(muscle[1]);
-      // emg_val[2] = analogRead(muscle[2]);
-      // emg_val[3] = analogRead(muscle[3]);
-
-      // (emg_val[0] > threshold[0]) ? Serial.println("Muscle 1 Activated!") : Serial.println("Muscle 1 NOT Activated!");
-      // (emg_val[1] > threshold[1]) ? Serial.println("Muscle 2 Activated!") : Serial.println("Muscle 2 NOT Activated!");
-      // (emg_val[2] > threshold[2]) ? Serial.println("Muscle 3 Activated!") : Serial.println("Muscle 3 NOT Activated!");
-      // (emg_val[3] > threshold[3]) ? Serial.println("Muscle 4 Activated!") : Serial.println("Muscle 4 NOT Activated!");
-      
-      messageChar.writeValue(String(emg_val[0]));
-      Serial.print("Destination Device Received: ");
-      Serial.println(messageChar.value());
-      
-      // Serial.println(emg_val[0]);
-      // Serial.print(',');
-      // Serial.println(emg_val[1]);
-      // Serial.print(',');
-      // Serial.print(emg_val[2]);
-      // Serial.print(',');
-      // Serial.print(emg_val[3]);
-      delay(10);
-    }
-
-    Serial.println("Disconnected.");
+    // Normalize
+    emg_norm[i] = (float)(emg_val[i] - emg_min[i]) / (emg_max[i] - emg_min[i]);
   }
+
+  // Compute signed balance: positive = front, negative = back
+  emg_delta[0] = emg_norm[0] - emg_norm[1];
+  // emg_delta[1] = 
+
+  // Map to angle:
+  // muscleDelta = -1 -> -45Deg (back contraction)
+  // MuscleDetla = 0 -> 0Deg (neutral)
+  // MuscleDelta = +1 -> 135Deg (front contraction)
+  
+  (emg_delta[0] >= 0) ? angle = emg_delta[0] * 135 : emg_delta[0] * 45;
+  // (emg_delta[1] >= 0) ? angle = emg_delta[1] * 135 : emg_delta[1] * 45;
+
+  Serial.println(emg_delta[0]);
+  // Serial.print(',');
+  // Serial.println(emg_delta[1]);
+  
+  // Serial.println(emg_val[0]);
+  // Serial.print(',');
+  // Serial.println(emg_val[1]);
+  // Serial.print(',');
+  // Serial.print(emg_val[2]);
+  // Serial.print(',');
+  // Serial.print(emg_val[3]);
+  delay(10);
 
 }
---
+++ src/ArmQuest_emg_ble/ArmQuest_emg_ble.ino
@@ -0,0 +1,107 @@
+#include <ArduinoBLE.h>
+
+#define EMG1 A0 // A0
+#define EMG2 15 // A1
+#define EMG3 16 // A2
+#define EMG4 17 // A3
+
+// Arduino Nano 33 BLE Rev3 MAC Address: 5B:86:37:1F:4E:7F
+
+// FUNCTIONS & OUTSIDE VARIABLES
+const int muscle[4]    = {EMG1, EMG2, EMG3, EMG4};
+float     emg_val[4]   = {0, 0, 0, 0};
+float     emg_norm[4]  = {0, 0, 0, 0};
+const int emg_min[4] = {50, 40, 0, 0}; // Adjust as needed can very depending on electrode placement
+const int emg_max[4] = {350, 600, 0, 0}; // Adjust as needed can very depending on electrode placement
+
+static float angle        = 0.0;
+static float emg_delta[2] = {0.0, 0.0};
+
+// Create a BLE service and a characteristic
+BLEService simpleService("180C"); // Custom service UUID
+BLEStringCharacteristic messageChar("2A56", BLERead | BLEWrite | BLENotify, 50); // Custom characteristic
+
+
+// INITIALIZATIONS
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+    while (1);
+  }
+
+  // Set the device name and local name
+  BLE.setDeviceName("Nano33BLE");
+  BLE.setLocalName("Nano33BLE");
+  BLE.setAdvertisedService(simpleService);
+
+  // Add characteristic to the service
+  simpleService.addCharacteristic(messageChar);
+
+  // Add service to BLE stack
+  BLE.addService(simpleService);
+
+  // Set initial value
+  messageChar.writeValue("Hello!");
+
+  // Start advertising
+  BLE.advertise();
+  Serial.println("BLE device is now advertising...");
+}
+
+// MAIN
+void loop() {
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    Serial.print("Connected to: ");
+    Serial.println(central.address());
+
+    while (central.connected()) {
+      emg_val[0] = analogRead(muscle[0]);
+      // emg_val[1] = analogRead(muscle[1]);
+      // emg_val[2] = analogRead(muscle[2]);
+      // emg_val[3] = analogRead(muscle[3]);
+
+      // Clamp values
+      for (int i = 0; i < 4; i++)
+      {
+        // Clamp values
+        emg_val[i] = constrain(emg_val[i], emg_min[i], emg_max[i]);
+
+        // Normalize
+        emg_norm[i] = (float)(emg_val[i] - emg_min[i]) / (emg_max[i] - emg_min[i]);
+      }
+
+      // Compute signed balance: positive = front, negative = back
+      emg_delta[0] = emg_norm[0] - emg_norm[1];
+      // emg_delta[1] = 
+
+      // Map to angle:
+      // muscleDelta = -1 -> -45Deg (back contraction)
+      // MuscleDetla = 0 -> 0Deg (neutral)
+      // MuscleDelta = +1 -> 135Deg (front contraction)
+      
+      (emg_delta[0] >= 0) ? angle = emg_delta[0] * 135 : emg_delta[0] * 45;
+      // (emg_delta[1] >= 0) ? angle = emg_delta[1] * 135 : emg_delta[1] * 45;
+      
+      messageChar.writeValue(String(emg_val[0]));
+      Serial.print("Destination Device Received: ");
+      Serial.println(messageChar.value());
+      
+      // Serial.println(emg_val[0]);
+      // Serial.print(',');
+      // Serial.println(emg_val[1]);
+      // Serial.print(',');
+      // Serial.print(emg_val[2]);
+      // Serial.print(',');
+      // Serial.print(emg_val[3]);
+      delay(10);
+    }
+
+    Serial.println("Disconnected.");
+  }
+
+}

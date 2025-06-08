3ff894eedeb262f7224695eda9da34993ba9b47a
Denise Tan <denisetanumihardja@yahoo.com>
2025-06-08 12:09:30 -0700

    Small changes

--- src/ArmQuest_emg/ArmQuest_emg.ino
+++ src/ArmQuest_emg/ArmQuest_emg.ino
@@ -1,4 +1,4 @@
-#define EMG1 A1
+#define EMG1 A0
 #define EMG2 A1
 #define EMG3 A2
 #define EMG4 A3 
@@ -27,7 +27,7 @@ void setup() {
 // MAIN
 void loop() {
   emg_val[0] = analogRead(muscle[0]);
-  // emg_val[1] = analogRead(muscle[1]);
+  emg_val[1] = analogRead(muscle[1]);
   // emg_val[2] = analogRead(muscle[2]);
   // emg_val[3] = analogRead(muscle[3]);
 
@@ -39,27 +39,27 @@ void loop() {
     // emg_avg[2] /= 10;
     // emg_avg[3] /= 10;
     
-    // Clamp values
-    for (int i = 0; i < 4; i++)
-    {
-      // Clamp values
-      emg_val[i] = constrain(emg_val[i], emg_min[i], emg_max[i]);
+    // // Clamp values
+    // for (int i = 0; i < 4; i++)
+    // {
+    //   // Clamp values
+    //   emg_val[i] = constrain(emg_val[i], emg_min[i], emg_max[i]);
 
-      // Normalize
-      emg_norm[i] = (float)(emg_val[i] - emg_min[i]) / (emg_max[i] - emg_min[i]);
-    }
+    //   // Normalize
+    //   emg_norm[i] = (float)(emg_val[i] - emg_min[i]) / (emg_max[i] - emg_min[i]);
+    // }
 
-    // Compute signed balance: positive = front, negative = back
-    emg_delta[0] = emg_norm[0] - emg_norm[1];
-    // emg_delta[1] = emg_norm[2] - emg_norm[3];
+    // // Compute signed balance: positive = front, negative = back
+    // emg_delta[0] = emg_norm[0] - emg_norm[1];
+    // // emg_delta[1] = emg_norm[2] - emg_norm[3];
 
-    // Map to angle:
-    // muscleDelta = -1 -> -45Deg (back contraction)
-    // MuscleDetla = 0 -> 0Deg (neutral)
-    // MuscleDelta = +1 -> 135Deg (front contraction)
+    // // Map to angle:
+    // // muscleDelta = -1 -> -45Deg (back contraction)
+    // // MuscleDetla = 0 -> 0Deg (neutral)
+    // // MuscleDelta = +1 -> 135Deg (front contraction)
     
-    (emg_delta[0] >= 0) ? angle = emg_delta[0] * 135 : emg_delta[0] * 45;
-    (emg_delta[1] >= 0) ? angle = emg_delta[1] * 135 : emg_delta[1] * 45;
+    // (emg_delta[0] >= 0) ? angle = emg_delta[0] * 135 : emg_delta[0] * 45;
+    // (emg_delta[1] >= 0) ? angle = emg_delta[1] * 135 : emg_delta[1] * 45;
 
     Serial.print(emg_avg[0]);
     Serial.print(',');
@@ -69,16 +69,18 @@ void loop() {
     // Serial.print(',');
     // Serial.print(emg_avg[3]);
 
-    Serial.println(emg_delta[0]);/
+    // Serial.println(emg_delta[0]);
     // Serial.print(',');
     // Serial.println(emg_delta[1]);
 
     count = 0;
+    emg_avg[0] = 0;
+    emg_avg[1] = 0;
   }
   else
   {
     emg_avg[0] += emg_val[0];
-    // emg_avg[1] += emg_val[1];
+    emg_avg[1] += emg_val[1];
     // emg_avg[2] += emg_val[2];
     // emg_avg[3] += emg_val[3];
     count++;
@@ -92,6 +94,6 @@ void loop() {
   // Serial.print(',');
   // Serial.print(emg_val[3]);
 
-  delay(10);
+  delay(5);
 
 }
